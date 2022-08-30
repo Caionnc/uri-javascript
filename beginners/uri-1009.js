25f58d87ceb6730004dd7cd95065dc823b8cf912
Caio Carvalho <caio.carvalho@lavid.ufpb.br>
2022-08-29 23:58:02 -0300

    URI 1008 and 1009

---
+++ beginners/uri-1009.js
@@ -0,0 +1,25 @@
+/*Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money). 
+Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month , 
+with two decimal places.
+
+- Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
+
+- Don’t forget the blank spaces.
+
+Input
+The input file contains a text (employee's first name), and two double precision values, that are the seller's salary and the total value sold by him/her.
+
+Output
+Print the seller's total salary, according to the given example. */
+
+var input = require("fs").readFileSync("stdin", "utf8");
+
+let values = input.split("\n");
+
+let name = String(values.shift());
+let fixedSalary = parseFloat(values.shift());
+let totalSells = parseFloat(values.shift());
+
+let finalSalary = fixedSalary+(15/100)*totalSells;
+
+console.log("TOTAL = R$ " + finalSalary.toFixed(2));
